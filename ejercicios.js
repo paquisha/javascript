5489286754fc756b452383641eb6c83953daa2bc
Andres Grijalva <paquishaxd@gmail.com>
2020-09-09 12:41:41 -0500

    segundo commit

--- ejercicios.js
+++ ejercicios.js
@@ -113,4 +113,8 @@
 //     }
 // }
 
-//
+//Realizar un programa que pida cargar una fecha cualquiera, luego verificar si dicha fecha corresponde a Navidad (se debe cargar por separado el dia, el mes y el año)
+
+let dia = prompt('Ingrese dia: ');
+let mes = prompt('Ingrese mes: ');
+let anio = prompt('Ingrese el año: ');
