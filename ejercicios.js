ba5fb289ab20756f5d088236d8f2bc5f24a5fd46
Andres Grijalva <paquishaxd@gmail.com>
2020-09-11 02:06:05 -0500

    Se agrega una hoja de html

--- ejercicios.js
+++ ejercicios.js
@@ -218,3 +218,31 @@
 //     }
 // }
 
+//Solicitar el ingreso alguna de estas palabras (casa, mesa, perro, gato) luego mostrar la palabra traducida en inglés. Es decir, si se ingresa 'casa' debemos mostrar el texto 'house' en la página.
+// let item = prompt('Ingrese alguna de estas palabras (casa, mesa, perro, gato)');
+// switch(item){
+//     case 'casa': 
+//         document.write('House');
+//         break;
+//     case 'mesa': 
+//         document.write('table');
+//         break;
+//     case 'perro':
+//         document.write('Dog');
+//         break;
+//     case 'gato':
+//         document.write('Cat');
+//         break;
+//     default:
+//         document.write('palabra no identificada');
+
+// }
+
+function toggle(source) {
+    checkboxes = document.getElementsByName('dinamico');
+  
+    for(var i=0, n=checkboxes.length;i<n;i++) {
+      checkboxes[i].checked = source.checked;
+    }
+  
+  }
