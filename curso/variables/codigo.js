7ca403f35a5b55632302d6995a13f6d9457f2001
paquisha <paquishaxd@gmail.com>
2021-12-07 19:57:23 -0500

    ejercicios de javascript para practicar

---
+++ curso/variables/codigo.js
@@ -0,0 +1,23 @@
+'use sctrict'
+
+
+//variables
+let nombre = 'manuel';
+let ci = 1234567890;
+
+//constantes
+const prueba = 'aqui una prueba';
+
+console.log('soy la constante: ' + prueba);
+
+console.log('soy una variable: ' + ci);
+
+
+//tipos de datos primiritvos
+/*
+string
+number
+boolean
+undefined
+null
+*/ 
